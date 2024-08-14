16c2676157a71491c42b3ab6dd26547bfdb90f73
Vtormacs <macskids98play@hotmail.com>
2024-08-14 11:14:06 -0300

    Criação

---
+++ src/main/java/com/Eventicket/EventicketApplication.java
@@ -0,0 +1,13 @@
+package com.Eventicket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventicketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Eventicket/EventicketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Eventicket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
