1de6dd5befb609b81ed126d98b59ebe375b05613
Vitor Eduardo Lopes Francisco <132294431+Vtormacs@users.noreply.github.com>
2024-08-16 01:02:23 -0300

    Revert "CRUD Basico de evento,Entity,Repository,Service,Controller"

--- src/main/java/com/Eventicket/Repositories/EventRepository.java
+++
@@ -1,7 +0,0 @@
-package com.Eventicket.Repositories;
-
-import com.Eventicket.Entities.EventEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EventRepository extends JpaRepository<EventEntity, Long> {
-}
