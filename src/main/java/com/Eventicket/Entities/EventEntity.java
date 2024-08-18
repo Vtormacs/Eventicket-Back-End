28e55c968c9e0f52614fcf413c48528cb4345b22
Felipe Luiz Wlodkowski <wlodfelipe@gmail.com>
2024-08-18 18:50:40 -0300

    Associação Event OneToMany Ticket

--- src/main/java/com/Eventicket/Entities/EventEntity.java
+++ src/main/java/com/Eventicket/Entities/EventEntity.java
@@ -10,6 +10,7 @@ import lombok.*;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
+import java.util.List;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -37,10 +38,11 @@ public class EventEntity {
     @NotEmpty
     private String descricao;
 
-//Fazer associção com Ingresso e Endereço
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "endereco_id")
     @JsonIgnoreProperties("eventos")
     private AddresEntity endereco;
 
+    @OneToMany(mappedBy = "evento", cascade = CascadeType.ALL)
+    private List<TicketEntity> ingresso;
 }
